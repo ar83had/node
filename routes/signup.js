420f76e69d178e63fcd420bcc376d0d6c5c4bd57
ar83had <arshadshmim786@gmail.com>
2024-05-24 20:48:00 +0530

    create route for login

--- routes/signup.js
+++ routes/signup.js
@@ -4,6 +4,8 @@ const func = require("../function/signup");
 
 
 routes.get("/signup",func.signup);
+routes.get("/login",func.login);
+routes.get("/login/store",func.login_store);
 routes.get("/signup/store",func.store);
 routes.get("/shortener/store",func.shorted_store);
 
