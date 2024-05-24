1fd0a82eec74c14dd550b381880c08073b485aa8
ar83had <arshadshmim786@gmail.com>
2024-05-24 19:10:36 +0530

    require shortener and store url shorted data

--- function/signup.js
+++ function/signup.js
@@ -2,6 +2,7 @@ const path  = require("path");
 const express = require("express");
 const app = express();
 const con = require("../connection/url_shortener");
+const shortner = require("node-url-shortener");
 
 app.set("view engin","ejs");
 app.set("views",path.resolve("../views"));
@@ -18,13 +19,7 @@ const  func =
             if(result.length == 0)
             {
                 con.query(`insert into url_signup values (\"${data.username}\",\"${data.password}\",\"${data.email}\")`);
-                con.query("select * from url_signup",(err,result)=>{
-                    if(err)
-                        console.log("err-->"+err);
-                    else
-                        res.render("demo.ejs",{"result":result});
-
-                })
+                res.render("home.ejs",{"email":data.email,"flag":0});
             }
             else
             {
@@ -34,6 +29,26 @@ const  func =
 
         });
 
+    },
+    shorted_store : function(req,res){
+        const data = req.query;
+        shortner.short(data.url,(err,url_shortener)=>{
+            con.query(`select url from url_shortener where url=\"${data.url}\"`,(err,re)=>{
+                console.log(re);
+                console.log(re.length);
+                if(re.length == 0)
+                {
+                    console.log(re.length);
+                    con.query(`insert into url_shortener values (\"${data.url}\",\"${url_shortener}\",0,\"${data.user_email}\")`);
+                }
+                con.query(`update url_shortener set view = view+1 where url = \"${data.url}\"`);
+            });
+        });
+
+        con.query(`select * from url_shortener where email=\"${data.user_email}\"`,(err,result)=>{
+            console.log(result);
+            res.render("home.ejs",{"flag":1,"result":result,"email":data.user_email});
+        })
     }
 }
 
